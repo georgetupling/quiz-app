761a118594b99b0d7b86e7d54ce8222b04964bf0
George Tupling <georgetupling@live.co.uk>
2023-02-10 11:57:36 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const { urlencoded } = require("express");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+app.listen(port = process.env.port || 3000, () => {
+    console.log(`Server is running on port ${port}.`)
+});
