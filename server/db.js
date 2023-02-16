89e0d30567feabff2f563cd29f57855f64aade21
George Tupling <georgetupling@live.co.uk>
2023-02-16 16:06:01 +0000

    Moves env variables to .env

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
+const dotenv = require("dotenv").config();
 
 // const uri = "mongodb://127.0.0.1:27017/quiz";
-const uri =
-  "mongodb+srv://george:myadItRRZxlSTNPC@cluster0.4q5sans.mongodb.net/quiz?retryWrites=true&w=majority";
+const uri = process.env.DB_URI;
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
